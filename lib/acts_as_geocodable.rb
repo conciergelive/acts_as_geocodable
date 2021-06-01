69b7734e695ecced3b8a38800b2ca4ca4685100b
Drew Ulmer <drew@unabridgedsoftware.com>
2021-06-01 17:12:17 -0500

    Add patch for Ruby 2.4+ compat
    Fixnum is deprecated in favor of Integer

--- lib/acts_as_geocodable.rb
+++ lib/acts_as_geocodable.rb
@@ -131,7 +131,7 @@ module CollectiveIdea #:nodoc:
           case location
           when Geocode then location
           when InstanceMethods then location.geocode
-          when String, Fixnum then Geocode.find_or_create_by_query(location)
+          when String, Integer then Geocode.find_or_create_by_query(location)
           end
         end
         
